3664c567dddaa10d166705449c57ceac06a4ebe8
Arjen de Aldrey <adealdrey3@gmail.com>
2024-12-21 12:39:35 +0100

    [ADD] VariableLists + examles

---
+++ Runtime/Variables/Lists/GameObjectListSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Ripple
+{
+    [RippleData]
+    [CreateAssetMenu(menuName = Config.VariableListMenu + "GameObject")]
+    public class GameObjectListSO : VariableListSO<GameObject>
+    {
+    }
+}
---
+++ Runtime/Variables/Lists/Vector2ListSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Ripple
+{
+    [RippleData]
+    [CreateAssetMenu(menuName = Config.VariableListMenu + "Vector2")]
+    public class Vector2ListSO : VariableListSO<Vector2>
+    {
+    }
+}
