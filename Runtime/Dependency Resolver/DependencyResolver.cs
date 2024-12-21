c12cfcdfb8baa7d986afd39e3264f4f6e115698a
Tristan Metz <tristan.a.metz@gmail.com>
2024-12-22 01:44:26 +0900

    Add Exception Handling for UltEvents dependency

--- Runtime/Dependency Resolver/DependencyResolver.cs
+++ Runtime/Dependency Resolver/DependencyResolver.cs
@@ -6,8 +6,11 @@ public static class DependencyResolver
 {
     static DependencyResolver()
     {
-#if !ODIN_INSPECTOR
+        #if !ODIN_INSPECTOR
         Debug.LogError("Odin Inspector is not installed in this project. If you want to be able to use Ripple you will have to install Odin Inspector");
-#endif
+        #endif
+        #if !ULTEVENTS
+        Debug.LogError("Ultevents is not installed in this project. If you want to be able to use Ripple you will have to install Ultevents");
+        #endif
     }
 }
