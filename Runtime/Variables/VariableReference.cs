1c1372a3fbc514d88b6a862f635244ec54eee060
Tristan Metz <tristan.a.metz@gmail.com>
2024-12-29 10:29:37 +0900

    hotfix 2

--- Runtime/Variables/VariableReference.cs
+++ Runtime/Variables/VariableReference.cs
@@ -35,20 +35,28 @@ namespace Ripple
     public class BoolReference : VariableReference<bool>
     {
         public BoolReference(bool val) : base(val) {}
+
+        public BoolReference() { }
     }
 
     [Serializable]
     public class FloatReference : VariableReference<float>
     {
         public FloatReference(float val) : base(val) {}
+
+        public FloatReference() { }
     }
     [Serializable] public class IntReference : VariableReference<int>
     {
         public IntReference(int val) : base(val) {}
+
+        public IntReference() { }
     }
     [Serializable] public class Vector3Reference : VariableReference<Vector3>
     {
         public Vector3Reference(Vector3 value) : base(value) { }
+
+        public Vector3Reference() { }
     }
     [Serializable] public class TransformReference : VariableReference<Transform> { }
     [Serializable] public class GameObjectReference : VariableReference<GameObject> { }
