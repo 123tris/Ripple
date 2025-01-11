2335ba1b5c5eda89c7a01a4f2a0c584ba12a97f9
Tristan Metz <tristan.a.metz@gmail.com>
2025-01-12 00:55:18 +0900

    Vector3 Event Listener

---
+++ Runtime/Events/Listeners/EventListenerVector3.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Ripple
+{
+    [AddComponentMenu(Config.EventListenerMenu + "Event Listener Vector3")]
+    public class EventListenerVector3 : EventListener<Vector3>
+    {
+        
+    }
+}
