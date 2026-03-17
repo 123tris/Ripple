fdbf9bab60da14fb1e6d0a732da35ad79e5f79a2
Tristan Metz <Tristan@avrjapan.co.jp>
2026-03-17 16:22:48 +0900

    [ADD] Add clear log button

--- Editor/EventLoggerWindow.cs
+++ Editor/EventLoggerWindow.cs
@@ -15,15 +15,31 @@ namespace Ripple
 
         void OnGUI()
         {
+            // Top toolbar
+            GUILayout.BeginHorizontal(EditorStyles.toolbar);
+            {
+                if (GUILayout.Button("Clear Logs", EditorStyles.toolbarButton))
+                {
+                    Logger.ResetLogs();
+                }
+
+                GUILayout.FlexibleSpace();
+            }
+            GUILayout.EndHorizontal();
+
+            // Scroll view
             scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
             var logs = Logger.GetLogs();
+
             for (int i = logs.Count - 1; i >= 0; i--)
             {
                 LogEntry logEntry = logs[i];
+
                 GUILayout.BeginHorizontal();
                 {
                     var size = GUI.skin.button.CalcSize(new GUIContent(logEntry.context.name));
                     size.x = Math.Max(100, size.x);
+
                     if (GUILayout.Button($"{logEntry.context.name}", GUILayout.Width(size.x), GUILayout.Height(size.y)))
                     {
                         Selection.activeObject = logEntry.context;
@@ -31,8 +47,9 @@ namespace Ripple
                         EditorGUIUtility.PingObject(logEntry.context);
                     }
 
-                    var labelStyle = EditorStyles.label;
+                    var labelStyle = new GUIStyle(EditorStyles.label); // avoid modifying shared style
                     labelStyle.richText = true;
+
                     GUILayout.Label(logEntry.message, labelStyle);
                 }
                 GUILayout.EndHorizontal();
